1886ed332474dd5f4a7a965a76178082c1ecdfb2
aftiyah <abd2010ly@gmail.com>
2020-06-30 12:22:51 +0200

    Styling Product Items.

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
-import productsReducer from './src/store/reducers/products';
-import ShopNavigator from './src/navigation/ShopNavigator';
+import productsReducer from "./src/store/reducers/products";
+import ShopNavigator from "./src/navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
 });
 
 const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <Provider  store={store}>
+    <Provider store={store}>
       <ShopNavigator />
     </Provider>
   );
--- src/navigation/ShopNavigator.js
+++ src/navigation/ShopNavigator.js
@@ -1,22 +1,26 @@
-import { createSwitchNavigator, createAppContainer } from 'react-navigation';
-import { Platform } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { Platform } from "react-native";
 
-import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
-import Colors from '../constants/Colors';
+import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+import Colors from "../constants/Colors";
 
-const ProductsNavigator = createSwitchNavigator({
-    ProductsOverview: ProductsOverviewScreen
-}, {
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen,
+  },
+  {
     defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
-        },
-        headerTineColor: Platform.OS === 'android' ? 'white' : Colors.primary
-    }
-});
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+    },
+  }
+);
 
 ProductsOverviewScreen.defaultNavigationOptions = {
-    headerTitle: 'All Products'
+  headerTitle: "All Products",
 };
 
-export default  createAppContainer(ProductsNavigator);
+export default createAppContainer(ProductsNavigator);
